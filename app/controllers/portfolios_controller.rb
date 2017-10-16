a1e2357859770b09140760041bc5d17763e5a10c
SahilThakur7 <sahilrd07@gmail.com>
2017-10-16 16:10:49 +0530

    CRUD for portfolio completed

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,13 @@ class PortfoliosController < ApplicationController
             end
         end
     end
+
+    def destroy
+        @portfolio_item = Portfolio.find(params[:id])
+
+        @portfolio_item.destroy
+        respond_to do |format|
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was deleted'}
+        end
+    end
 end
