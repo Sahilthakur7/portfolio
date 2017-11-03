87f90a5293cef3fda06f017f3b8cd23fbebc5d93
SahilThakur7 <sahilrd07@gmail.com>
2017-11-03 17:44:17 +0530

    Nested forms started

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
     includes Placeholder
     has_many :technologies
+    accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank?}
 
     validates_presence_of :title, :main_image
     after_initialize :set_defaults
