66f27cc26a0e6c9830afd3b5326823519eef2832
SahilThakur7 <sahilrd07@gmail.com>
2017-12-01 11:40:27 +0530

    refactoring the styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,6 @@ module ApplicationHelper
         if current_user.is_a?(GuestUser)
             (link_to "Sign-up", new_user_registration_path, class: style)+ " ".html_safe +
                 (link_to "Login", new_user_session_path, class: style) 
-
-
         else
             link_to "Logout", destroy_user_session_path,method: :delete, class: style
 
@@ -21,6 +19,18 @@ module ApplicationHelper
 
     def copyright_generator
         SahilViewTool::Renderer.copyright("Sahil Thakur", "All rights reserved")
+    end
+
+    def nav_helper(style="",tag_type="")
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_me_path}" class="#{style}">About Me</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style}">Blog</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style}">Portfolio</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style}">Contact</a></#{tag_type}>
+NAV
+
+    nav_links.html_safe
     end
 
 end
