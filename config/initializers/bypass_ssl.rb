dacb0a239f6f75ff64d3532f1c36dfd4facd3a72
SahilThakur7 <sahilrd07@gmail.com>
2018-01-14 08:38:17 +0530

    Twitter problem solved

--- config/initializers/bypass_ssl.rb
+++ config/initializers/bypass_ssl.rb
@@ -1,1 +1,4 @@
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+prev_setting = OpenSSL::SSL.send(:remove_const, :VERIFY_PEER)
+OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
+OpenSSL::SSL.send(:remove_const, :VERIFY_PEER)
+OpenSSL::SSL.const_set(:VERIFY_PEER, prev_setting)
